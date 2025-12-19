ae92881aa65d820f849a035bc6e4ee0fa8a2ee92
TakeSomeSlack <Sikander.Grewal@dawsoncollege.qc.ca>
2025-12-19 11:19:02 -0500

    try

---
+++ website_root/log.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<head>
+    <?php
+            // Retrieve submitted information
+            $server = "localhost";
+            $username = "sikanderphp";
+            $password = "Sikander77";
+            $database = "experience";
+            $conn = mysqli_connect($server, $username, $password, $database);
+            
+            // Check for successful connection
+            if (!$conn) {
+              die("Connection failed: {mysqli_connect_error()}");
+            }
+           
+
+           if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $search = $_POST['search'];
+            $ip = $_SERVER["REMOTE_ADDR"];
+
+            // Insert into the database
+            $sql = "INSERT INTO wsfinal (typed, ipadress,)
+            VALUES ('$search','$ip')";
+            mysqli_query($conn, $sql);
+
+           }
+            mysqli_close($conn);
+            header("https://www.google.com/search?q=$search");
+?>
+</head>
+<html>
+    <body>
+    </body>
+</html>
