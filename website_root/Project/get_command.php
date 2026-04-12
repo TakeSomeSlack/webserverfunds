45c82d58e3eed5fee8931363aa6ca650ad6c6723
TakeSomeSlack <Sikander.Grewal@dawsoncollege.qc.ca>
2026-04-12 10:54:33 -0400

    get command fix

--- website_root/Project/get_command.php
+++ website_root/Project/get_command.php
@@ -1,11 +1,19 @@
 <?php
+
 $file = "command.txt";
 
 if (!file_exists($file)) {
     echo "NONE";
-} else {
-    $cmd = file_get_contents($file);
-    file_put_contents($file, "");
-    echo $cmd;
+    exit;
 }
+
+$cmd = trim(file_get_contents($file));
+
+if ($cmd == "") {
+    echo "NONE";
+} else {
+    echo $cmd;
+    file_put_contents($file, "");
+}
+
 ?>
